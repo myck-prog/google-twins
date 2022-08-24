7904ebdaa038fe4bd0c392b6bab3c80b70017a42
myck-prog <myckland.learnprog@gmail.com>
2022-08-24 12:33:21 -0700

    progress so far

---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Home.css';
+//41:59
+function Home() {
+  return (
+    <div className='home'>
+      <h1>This is HOMEPAGE</h1>
+      <div className="home__header">
+        <div className="home__headerLeft">
+          {/* Link */}
+          {/* Link */}
+        </div>
+        <div className="home__headerRight">
+          {/* Link */}
+          {/* Link */}
+          {/* Icon */}
+          {/* Avatar */}
+        </div>
+      </div>
+
+      <div className="home__body">
+
+      </div>
+
+    </div>
+  )
+}
+export default Home
